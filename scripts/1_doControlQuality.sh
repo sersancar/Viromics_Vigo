4a940380e4fdb73a2d8b7172d8aa579c2216d04c
Sergio Sánchez Carrillo <sergiosanchezcarrillo@gmail.com>
2024-10-19 17:34:32 +0200

    Update 1_doControlQuality.sh

--- scripts/1_doControlQuality.sh
+++ scripts/1_doControlQuality.sh
@@ -11,10 +11,15 @@ OUTPUTDIR=$LUSTRE/QualityResults
 # Create output directory if it doesn't exist
 if [ ! -d ${OUTPUTDIR} ];then mkdir -p ${OUTPUTDIR};fi
 
+# Run it with error handling
+set -e  # Exit immediately if a command exits with a non-zero status
+
 # Run your application
 fastqc ${INPUTDIR}/*.fastq.gz -t $SLURM_CPUS_PER_TASK -o ${OUTPUTDIR}
 cd ${OUTPUTDIR}
 multiqc ${OUTPUTDIR}/.
+
+
 
 # Run => ./doQualityControl.sh
 # Help 1 => module spider fastqc/0.12.1 

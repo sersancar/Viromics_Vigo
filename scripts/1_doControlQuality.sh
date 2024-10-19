ab7c758c3c999d1264602ecb777dc178f1d5f522
Sergio Sánchez Carrillo <sergiosanchezcarrillo@gmail.com>
2024-10-19 17:35:23 +0200

    Update 1_doControlQuality.sh

--- scripts/1_doControlQuality.sh
+++ scripts/1_doControlQuality.sh
@@ -19,7 +19,13 @@ fastqc ${INPUTDIR}/*.fastq.gz -t $SLURM_CPUS_PER_TASK -o ${OUTPUTDIR}
 cd ${OUTPUTDIR}
 multiqc ${OUTPUTDIR}/.
 
-
+# Check if the run was successful
+if [ $? -eq 0 ]; then
+    echo "Script completed successfully."
+else
+    echo "Script failed." >&2
+    exit 1
+fi
 
 # Run => ./doQualityControl.sh
 # Help 1 => module spider fastqc/0.12.1 

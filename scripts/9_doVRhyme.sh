26ca2efa291ece3718c56f66c99646a7fcde763d
Sergio Sánchez Carrillo <sergiosanchezcarrillo@gmail.com>
2024-10-20 13:43:20 +0200

    Create 9_doVRhyme.sh

---
+++ scripts/9_doVRhyme.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Load necessary modules
+module load cesga/system miniconda3/22.1
+
+# Activate environment
+conda activate vRhyme
+
+# Directories 
+DATADIR=$LUSTRE/sergio/viroSeqs
+READSDIR=$LUSTRE/sergio/filteredReads
+OUTPUTDIR=$LUSTRE/sergio/vRhymeResults
+
+# Run it with error handling
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Run your application
+vRhyme -i ${DATADIR}/total_viral_contigs.fa \
+-r ${READSDIR}/*.fastq \
+-o ${OUTPUTDIR} \
+--method composite \
+-t $SLURM_CPUS_PER_TASK \
+--iter 20 \
+-l 2000 \
+--bin_size 2 \
+-m 1000 
+
+# Check if the job was successful
+if [ $? -eq 0 ]; then
+    echo "Job completed successfully."
+else
+    echo "Job failed." >&2
+    exit 1
+fi
+
+# Deactivate the environment
+conda deactivate
+
+# RUN => ./9_doVRhyme.sh
+# HELP => vRhyme -h

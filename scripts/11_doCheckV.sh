1fa852d3e4a0441c944d0f50cb43e71afbf50dc3
Sergio Sánchez Carrillo <sergiosanchezcarrillo@gmail.com>
2024-10-20 13:52:32 +0200

    Create 11_doCheckV.sh

---
+++ scripts/11_doCheckV.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Directories
+DATADIR=$LUSTRE/sergio/viroSeqs
+OUTDIR=$LUSTRE/sergio/checkVBinsResults
+
+#Create OUTDIR if it does not exist
+if [ ! -d $OUTDIR ]; then mkdir -p $OUTDIR; fi
+
+# Load necessary modules
+module load cesga/2020 gcccore/system checkv/1.0.1
+
+# Run it with error handling
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Run your application
+checkv end_to_end ${DATADIR}/totalBinnedContigs.fa ${OUTDIR} \
+-t $SLURM_CPUS_PER_TASK --remove_tmp
+
+# Check if the job was successful
+if [ $? -eq 0 ]; then
+    echo "Job completed successfully."
+else
+    echo "Job failed." >&2
+    exit 1
+fi
+
+# Help module spider checkv/1.0.1
+# ./doCheckV.sh

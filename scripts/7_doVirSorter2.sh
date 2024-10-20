ec335626da6765bd478ee82eb6222285ec2a1055
Sergio Sánchez Carrillo <sergiosanchezcarrillo@gmail.com>
2024-10-20 12:27:22 +0200

    Create 7_doVirSorter2.sh

---
+++ scripts/7_doVirSorter2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Load necessary modules
+module load cesga/system virsorter2/2.2.4
+
+# Directories 
+INPUTDIR=$LUSTRE/sergio/MegahitResults
+OUTPUTDIR=$LUSTRE/sergio/VS2Results
+
+# Create output directory if it doesn't exist
+if [ ! -d ${OUTPUTDIR} ];then mkdir -p ${OUTPUTDIR};fi
+
+# Run it with error handling
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Run your application
+virsorter config --init-source --db-dir=$EBROOTVIRSORTER2/db
+
+virsorter run -w ${OUTPUTDIR} \
+-i  ${INPUTDIR}/total_filtered_contigs.fa \
+-j  $SLURM_CPUS_PER_TASK \
+--tmpdir ${OUTPUTDIR}/tmp \
+--rm-tmpdir \
+--include-groups dsDNAphage,NCLDV,RNA,ssDNA,lavidaviridae
+
+# Check if the job was successful
+if [ $? -eq 0 ]; then
+    echo "Job completed successfully."
+else
+    echo "Job failed." >&2
+    exit 1
+fi
+
+# RUN => ./7_doVirSorter2.sh
+# HELP => module spider cesga/system virsorter2/2.2.4
